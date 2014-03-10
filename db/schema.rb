fa9b56f1c23b6c0c971bf397af157678a354752c
madhukosuri <you@example.com>
2014-03-10 11:39:41 +0530

    modify student and student and district

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131217103950) do
+ActiveRecord::Schema.define(version: 20140310060347) do
 
   create_table "active_admin_comments", force: true do |t|
     t.string   "namespace"
@@ -83,6 +83,7 @@ ActiveRecord::Schema.define(version: 20131217103950) do
     t.string   "image_content_type"
     t.integer  "image_file_size"
     t.datetime "image_updated_at"
+    t.string   "email"
   end
 
   create_table "teachers", force: true do |t|
